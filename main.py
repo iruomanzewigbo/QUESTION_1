52b79823cfe08c6f1f09a19e6bc1b1d52fc5dedf
Iruoma C.N <iruomanzewigbo@gmail.com>
2025-02-28 08:53:08 +0100

    Commit new files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,redirect,url_for,render_template,request
+import config
 
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
@@ -9,4 +10,4 @@ def home():
     return render_template('index.html')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=config.PORT,host=config.HOST,debug=True)
